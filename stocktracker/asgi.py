ae1e9972edfa2d1367130d79de62434a53aaf152
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-09-05 22:58:38 +0600

    Update asgi.py

--- stocktracker/asgi.py
+++ stocktracker/asgi.py
@@ -2,13 +2,22 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 import mainapp.routing
+from channels.auth import AuthMiddlewareStack
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stocktracker.settings')
+
+# application = ProtocolTypeRouter({
+#     'http': get_asgi_application(),
+#     'websocket': URLRouter(
+#         mainapp.routing.websocket_urlpatterns 
+        
+#     )
+    
+# })
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': URLRouter(
+    'websocket':AuthMiddlewareStack( URLRouter(
         mainapp.routing.websocket_urlpatterns 
-        
     )
-    
+    )
 })
