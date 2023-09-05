6c4c625bfef09fb81dbceab646e0803ca2163e45
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-09-05 22:58:23 +0600

    Update consumers.py

--- mainapp/consumers.py
+++ mainapp/consumers.py
@@ -3,46 +3,63 @@ from channels.exceptions import StopConsumer
 from time import sleep
 import asyncio
 import json
-from asgiref.sync import async_to_sync
-from asgiref.sync import sync_to_async
-from channels.db import database_sync_to_async
+from asgiref.sync import async_to_sync, sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
+from urllib.parse import parse_qs
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
+import copy
 
-class MySyncConsumer(SyncConsumer):
-    def websocket_connect(self, event):
-        print("connected .....", event)
-        async_to_sync (self.channel_layer.group_add)(
-            "programmers", self.channel_name
-        )
-        self.send({
-            'type':'websocket.accept',
-        })
 
-    def websocket_receive(self, event):
-        print("receiving riad.....", event['text'])
-        async_to_sync (self.channel_layer.group_send)(
-            'programmers', 
+class StockConsumer(AsyncWebsocketConsumer):
+
+    @sync_to_async
+    def addToCeleryBeat(self, stockpicker):
+        task = PeriodicTask.objects.filter(name = "every-10-seconds")
+        if len(task)>0:
+            print("hello")  # testing that task.first() will work or not
+            task = task.first()
+            args = json.loads(task.args)
+            args = args[0]
+            for x in stockpicker:
+                if x not in args:
+                    args.append(x)
+            task.args = json.dumps([args])
+            task.save()
+        else:
+            schedule, created = IntervalSchedule.objects.get_or_create(every=10, period = IntervalSchedule.SECONDS)
+            task = PeriodicTask.objects.create(interval = schedule, name='every-10-seconds', task="mainapp.tasks.update_stock", args = json.dumps([stockpicker]))
+    
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"stock_{self.room_name}"
+
+        # Join room group
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
             {
-                'type':'chat.message',
-                'message': event['text']
+                "type": "stock.update",
+                "message": message
+                
             }
         )
-    def chat_message(self, event):
-        print("Event 55 .. ",event)
-        print('Actual Data .. ', event['message'])
-        self.send({
-            'type':'websocket.send',
-            'text': event['message'],
-        })
 
-    def websocket_disconnect(self, event):
-        print("Channel Layer ..", self.channel_layer)
+    # Receive message from room group
+    async def stock_update(self, event):
+        message = event["message"]
 
-        print("Channel Name ..", self.channel_name)
-
-        async_to_sync (self.channel_layer.group_discard)(
-            "programmers", self.channel_name
-        )
-
-        print("disconnected .....", event)
-        raise StopConsumer()
-
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message}))
